c613a83542b7cc64184cfa8e4206813e6100f747
Semi <snehasah.22052005@gmail.com>
2026-06-25 10:24:16 +0530

    Create Pattern_20.cpp

---
+++ Patterns/Pattern_20.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n; cin >> n; int num = 1;
+    for(int i = 1; i <= 2 * n - 1; i++){
+        //star
+        for(int j = 1; j <= num; j++){
+            cout << "*";
+        }
+        //space
+        for(int k = 1; k <= (n - num + 1) * 2; k++){
+            cout << " ";
+        }
+
+        //star
+        for(int j = 1; j <= num; j++){
+            cout << "*";
+        }
+
+        if (i < n) num++;
+        else num--;
+
+        cout << endl;
+    }
+}
